2624c3fdd1b1ecd4c4fd331353af9f978f90feb5
kiba.x.zhao <kiba.rain@qq.com>
2020-11-20 21:44:42 +0800

    添加router

---
+++ boilerplate/app/router.js
@@ -0,0 +1,11 @@
+/**
+ * @fileOverview 路由配置
+ * @name router.js
+ * @author kiba.x.zhao <kiba.rain@qq.com>
+ * @license MIT
+ */
+'use strict';
+
+module.exports = app => {
+  const { router, controller } = app;
+};
