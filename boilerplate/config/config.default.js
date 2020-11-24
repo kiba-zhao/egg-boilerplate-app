0a9f351bf13f6914d08f35e5d319beaa1efe372f
kiba.x.zhao <kiba.rain@qq.com>
2020-11-24 17:30:41 +0800

    注释benchmark,logging

--- boilerplate/config/config.default.js
+++ boilerplate/config/config.default.js
@@ -27,13 +27,13 @@ module.exports = appInfo => {
   config.sequelize = {
     dialect: 'mysql', // support: mysql, mariadb, postgres, mssql
     connectionUri: `mysql://root:example@127.0.0.1:3306/${appInfo.name}`,
-    benchmark: true,
-    logging(...args) {
-      // if benchmark enabled, log used
-      const used = typeof args[1] === 'number' ? args[1] : undefined;
-      if (!used) { appInfo.logger.info(`[egg-sequelize] ${args[0]}`); } else if (used > 60000) { appInfo.logger.warn(`[egg-sequelize]${used}ms ${args[0]}`); } else { appInfo.logger.info(`[egg-sequelize]${used}ms ${args[0]}`); }
+    // benchmark: true,
+    // logging(...args) {
+    //   // if benchmark enabled, log used
+    //   const used = typeof args[1] === 'number' ? args[1] : undefined;
+    //   if (!used) { appInfo.logger.info(`[egg-sequelize] ${args[0]}`); } else if (used > 60000) { appInfo.logger.warn(`[egg-sequelize]${used}ms ${args[0]}`); } else { appInfo.logger.info(`[egg-sequelize]${used}ms ${args[0]}`); }
 
-    },
+    // },
     // delegate: 'myModel', // load all models to `app[delegate]` and `ctx[delegate]`, default to `model`
     // baseDir: 'my_model', // load all files in `app/${baseDir}` as models, default to `model`
     // exclude: 'index.js', // ignore `app/${baseDir}/index.js` when load models, support glob and array
